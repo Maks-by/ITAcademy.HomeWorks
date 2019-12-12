649a7544b5e8ef6f6bdc522b150ad12a5948dee7
Maks-by <58811835+Maks-by@users.noreply.github.com>
2019-12-12 16:01:39 +0300

    Add files via upload

---
+++ HW.02.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.IO;
+namespace Blanc_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader textReader = new StreamReader(@"C:\Users\Максим\Desktop\DZ\DZ2\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+            imageBytes[i] = binary;
+            }
+            File.WriteAllBytes(@"C:\Users\Максим\Desktop\DZ\DZ2\imaje.png", imageBytes);
+            textReader.Dispose();
+        }
+    }
+}
